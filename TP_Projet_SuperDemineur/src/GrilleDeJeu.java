aab315d20d98d234f507fe89829333d78c428afd
Hélias Essabar <helias.essabar@epfedu.fr>
2025-12-17 11:02:07 +0100

    toutescellulesrevelles finie

--- TP_Projet_SuperDemineur/src/GrilleDeJeu.java
+++ TP_Projet_SuperDemineur/src/GrilleDeJeu.java
@@ -116,7 +116,7 @@ public boolean getPresence(int i, int j){
 public boolean toutesCellulesRevelees(){
     for(int i=0 ;i<nbLines; i++){
         for (int j=0; j<nbColonnes; j++){
-            if(!matriceCellules[i][j].isPresenceBombe()) && !matriceCellules[i][j].isrevellerCellule()){
+            if(!matriceCellules[i][j].isPresenceBombe() && !matriceCellules[i][j].estDevoilee()){
             return false;
             }
         }
